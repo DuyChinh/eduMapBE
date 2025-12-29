cf697be6e54511884590ff8050c5af64d979f3e6
DuyChinh <doanduychinh2102@gmail.com>
2025-12-29 21:09:54 +0700

    [main] fix forgot password onrender

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -2,7 +2,9 @@ const nodemailer = require('nodemailer');
 
 const createTransporter = () => {
   return nodemailer.createTransport({
-    service: 'gmail', // Hoặc service khác như 'outlook', 'yahoo'
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // true for 465, false for others
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS // App password cho Gmail
